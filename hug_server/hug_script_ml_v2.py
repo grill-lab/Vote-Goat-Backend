e7315174e8fe80e8a606c8a8c91c515026501106
grcgrc <grctestemail@gmail.com>
2018-06-16 17:56:47 +0100

    Updated template for ML recommendation HUG function

--- hug_server/hug_script_ml_v2.py
+++ hug_server/hug_script_ml_v2.py
@@ -18,7 +18,7 @@ db = client.popcorndb
 # Declare the Google Cloud Model here?
 
 #link imdbID to movielens
-base_folder = './ml-latest-small'
+base_folder = './ml-20m' # NOTE: The 20m links.csv is only 250KB!
 links = pandas.read_csv(base_folder +'/links.csv', engine='python', sep=',') # Retrieving the 'links' csv file to match tmdb:imdb ids.
 prepend_imdb_tag = 'tt' #Required prepended tag
 prepended_imdbID = links['imdbId'].apply(lambda x: prepend_imdb_tag + str(int(x)).zfill(7)) # Creating the same imdbID format which are present in mongodb
@@ -56,8 +56,22 @@ def check_api_token(api_key):
     else:
         return False
 
+#########################################
+## Only focus on the following 2
+
+function ml_movie_recomendation (QUERY_USER_ID, QUERY_RATED_MOVIE_IDS, QUERY_RATED_MOVIE_SCORES, QUERY_RATED_GENRE_IDS, QUERY_RATED_GENRE_FREQS, QUERY_RATED_GENRE_AVG_SCORES):
+    """
+    Produces a list of movie recommendations (IMDB IDs)
+    https://github.com/GoogleCloudPlatform/cloudml-samples/blob/master/movielens/trainer/task.py#L63
+    """
+    movie_ids = []
+
+    # @Jeff TODO: Insert Google cloud ml request code.
+
+    return movie_ids
+
 @hug.get(examples='gg_id=anonymous_google_id&api_key=API_KEY')
-def get_nn_list(gg_id: hug.types.text, api_key: hug.types.text, hug_timer=120):
+def ml_movie_recommendation(gg_id: hug.types.text, api_key: hug.types.text, hug_timer=120):
     """
     Input: gg_id, api_key
     Output: Ten NN predicted top-k movies!
@@ -72,46 +86,47 @@ def get_nn_list(gg_id: hug.types.text, api_key: hug.types.text, hug_timer=120):
             # TODO: The following line was used to limit our mongodb moviedb to only the movies present in the 'small' movielens dataset. Likely not needed anymore? Limit to the larger links CSV?
             #   rating_training = rating_training[rating_training['imdbID'].isin(links_dict.keys())] # Limiting the pandas array to movies present in the links.csv file
 
-            #rating_training["imdbID"].replace(links_dict, inplace=True) # Replacing the imdbIDs (tt0000001) with movielens tmdb ids
-
-            #prepare input of model
-            vote = np.array(np.int_(rating_training.rating))
-            movieId = np.int_(np.array(rating_training.imdbID))
-
-            """
-            TODO: @Jeff: Insert Google cloud ml request code.
-
-            HUG INPUTS:
-
             # The user id.
-            QUERY_USER_ID = 'query_user_id'
-            # The ids of movies rated by the user.
-            QUERY_RATED_MOVIE_IDS = 'query_rated_movie_ids'
-            # The scores on the rated movies given by the user.
-            QUERY_RATED_MOVIE_SCORES = 'query_rated_movie_scores'
-            # The set of genres of the rated movies.
-            QUERY_RATED_GENRE_IDS = 'query_rated_genre_ids'
-            # The number of times the user rated each genre.
-            QUERY_RATED_GENRE_FREQS = 'query_rated_genre_freqs'
-            # The average rating on each genre.
-            QUERY_RATED_GENRE_AVG_SCORES = 'query_rated_genre_avg_scores'
+            QUERY_USER_ID = gg_id #'query_user_id'
 
-            Create a hug function to call the Google cloud ML, using the above input data!
-
-            The above variables will require pymongo calls to the mongodb tables.
-
-            create k movie list
-            get top k movie id
             """
-            k = 10
-            k_movie_id = sort_index[0,-k:]
-            k_imdb_id = list(map(reverse_dict.get, k_movie_id)) # Converting the top-k list movie IDs from movielens/tmdb back to imdbID
+            # NOTE: Movielens movie rating dataset uses different id from IMDB.
+            # @Jeff - Should we either:
+            # * Pre-process the movielens dataset & change the google cloud movie recommendation repo code to use IMDB Ids instead of movielens Ids?
+            # * Just conver the IMDB Ids here to Movielens Ids using the following code:
+            # rating_training["imdbID"].replace(links_dict, inplace=True) # Replacing the imdbIDs (tt0000001) with movielens tmdb ids
+            # The ids of movies rated by the user.
+            """
+            QUERY_RATED_MOVIE_IDS = 'query_rated_movie_ids' # ['id', 'id', ...]
+
+            # The scores on the rated movies given by the user.
+            QUERY_RATED_MOVIE_SCORES = 'query_rated_movie_scores' # [1, 0, ...]
+
+            # Implemented Vote Goat genre names
+            # TODO: Allocate IDs to the following genres
+            Genre_Names = ['Thriller', 'Drama', 'Crime', 'Romance', 'Horror', 'Short', 'Western', 'Musical', 'Mystery', 'Fantasy', 'Animation', 'Comedy', 'Biography', 'Action', 'Adventure', 'Sci-Fi', 'Sport', 'Documentary', 'Film-Noir', 'Family', 'War']
+
+            # The set of genres of the rated movies.
+            QUERY_RATED_GENRE_IDS = 'query_rated_genre_ids' # TODO: ID? 'genre' || '001' # [1, 2, ...]
+
+            # The number of times the user rated each genre.
+            QUERY_RATED_GENRE_FREQS = 'query_rated_genre_freqs' # [32, 2, ...]
+
+            # The average rating on each genre.
+            QUERY_RATED_GENRE_AVG_SCORES = 'query_rated_genre_avg_scores' # TODO: Average rating between 0 & 1, or upvotes/upvotes+downvotes ? # [int, int, ...]
+
+            # Producing a list of movie recommendations
+            recomended_movie_ids = ml_movie_recomendation(QUERY_USER_ID, QUERY_RATED_MOVIE_IDS, QUERY_RATED_MOVIE_SCORES, QUERY_RATED_GENRE_IDS, QUERY_RATED_GENRE_FREQS, QUERY_RATED_GENRE_AVG_SCORES)
+
+            # NOTE: Perhaps best to perform top-k reduction before the next step?
+            recomended_movie_ids = list(map(reverse_dict.get, recomended_movie_ids)) # Converting the top-k list movie IDs from movielens/tmdb back to imdbID
 
             combined_json_list = [] # Empty list where we'll store the top-k movie json items
             currentTime = pendulum.now() # Getting the time
             timestamp = int(round(currentTime.timestamp())) # Converting to timestamp, rounding it & converting to int
 
-            for movie_id in k_imdb_id: # Loop over the top-k results
+            for movie_id in recomended_movie_ids: # Loop over the top-k results
+
                 result = db.movie.find_one({"imdbID": movie_id}) # Retrieve movie given the imdb ID from the top-k list
 
                 combined_json_list.append({'imdbID': result['imdbID'],
@@ -130,7 +145,7 @@ def get_nn_list(gg_id: hug.types.text, api_key: hug.types.text, hug_timer=120):
 
             clicked_movie_IDs = [] # Intentionally blank!
             voting_intention = [] # Intentionally blank!
-            ML_ID = "Google Cloud Model"
+            ML_ID = "Google Cloud Model" # TODO: Rename automatically to latest model name somehow?
 
             db.recommendation_history.insert_one({"userId": user_id, "k_movie_list": k_imdb_id, "ML_ID": files[0], "k_mov_timestamp": timestamp, "clicked_movie_IDs": clicked_movie_IDs, "voted": voting_intention})
 
